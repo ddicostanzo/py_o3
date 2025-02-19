1a51c2f180355b8b75a37b8c40f733ae60cb20b0
Dominic DiCostanzo <dicostanzo.2@osu.edu>
2025-02-19 13:51:57 -0500

    updated for mananging mislabeled reference system fields

--- src/base/o3_attribute.py
+++ src/base/o3_attribute.py
@@ -12,6 +12,20 @@ class O3Attribute(O3Element):
         self.reference_system_for_values = item_dict['ReferenceSystemForValues']
         self.allow_null_values = item_dict['AllowNullValues']
         self.value_example = item_dict['ValueExample']
+        self.__check_reference_system(item_dict)
+        self.__clean_standard_values_list()
+
+    def __check_reference_system(self, item_dict):
+        if self.reference_system_for_values is None:
+            if any(['Reference System' in x for x in item_dict['StandardValuesList']]):
+                for sv in item_dict['StandardValuesList']:
+                    if 'Reference System' in sv:
+                        self.reference_system_for_values = sv.split(': ')[-1].split('{')[0].strip()
+                        break
+
+    def __clean_standard_values_list(self):
+        if len(self.standard_values_list) == 1 and 'Reference System' in self.standard_values_list[0].value_name:
+            self.standard_values_list.pop(0)
 
 
 if __name__ == "__main__":
