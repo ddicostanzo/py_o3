e578db872255f6637f722e0b8c0c8eeb14eedff8
Dominic DiCostanzo <dicostanzo.2@osu.edu>
2025-02-21 16:43:13 -0500

    Updated attribute class

--- src/base/o3_attribute.py
+++ src/base/o3_attribute.py
@@ -1,5 +1,6 @@
 from base.o3_standard_value import O3StandardValue
 from base.o3_element import O3Element
+import warnings
 
 
 class O3Attribute(O3Element):
@@ -14,6 +15,12 @@ class O3Attribute(O3Element):
         self.value_example = item_dict['ValueExample']
         self.__check_reference_system(item_dict)
         self.__clean_standard_values_list()
+        self.__clean_value_data_types()
+        self.__sql_data_types = {"Boolean": "bit",
+                                 "DICOM Image": "varbinary",
+                                 "Date": "datetime2",
+                                 "Decimal": "decimal("
+                                 }
 
     def __check_reference_system(self, item_dict):
         if self.reference_system_for_values is None:
@@ -30,6 +37,25 @@ class O3Attribute(O3Element):
     @property
     def sql_field_name(self):
         return ''.join(self.string_code.split('_')[1:])
+
+    def __clean_value_data_types(self):
+
+        if len(self.standard_values_list) > 0:
+            self.value_data_type = "String"
+        elif len(self.standard_values_list) == 0 and self.value_data_type == "":
+            self.value_data_type = "String"
+            warnings.warn(f"Setting value data type to string for attribute: {self.value_name}.", UserWarning)
+
+        if self.value_data_type not in ['Boolean', 'DICOM Image', 'Date', 'Decimal', 'Integer', 'String']:
+            if self.value_data_type == "Int":
+                self.value_data_type = "Integer"
+            if self.value_data_type == "Numeric":
+                self.value_data_type = "Decimal"
+            if "Date" in self.value_name:
+                self.value_data_type = "Date"
+            if self.value_data_type == "string":
+                self.value_data_type = "String"
+
 
 if __name__ == "__main__":
     pass
