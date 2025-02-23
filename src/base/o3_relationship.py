73cd3896daeade97b9953eef215d2f532247b201
Dominic DiCostanzo <ddicostanzo@gmail.com>
2025-02-23 08:48:09 -0500

    Code cleanup

--- src/base/o3_relationship.py
+++ src/base/o3_relationship.py
@@ -11,5 +11,6 @@ class O3Relationship:
     def __str__(self):
         return f'{self.subject_element} {self.relationship_category} {self.predicate_element}'
 
+
 if __name__ == '__main__':
     pass
