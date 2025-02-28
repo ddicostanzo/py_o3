6a186c4910a0b039659ffa08b43c5f3bc8e32b55
Dominic DiCostanzo <dicostanzo.2@osu.edu>
2025-02-28 10:38:42 -0500

    Column SQL generation

--- src/base/o3_attribute.py
+++ src/base/o3_attribute.py
@@ -10,30 +10,17 @@ class O3Attribute(O3Element):
 
         self.__possible_value_data_types = ['Boolean', 'Binary', 'Date', 'Decimal', 'Integer', 'String']
         self.value_data_type = item_dict['ValueDataType']
-        self.sql_data_type = None
         self.standard_values_use = item_dict['StandardValuesUse']
         self.standard_values_list = item_dict['StandardValuesList']
         self.standard_values_list = [O3StandardValue(x) for x in self.standard_values_list]
         self.reference_system_for_values = item_dict['ReferenceSystemForValues']
         self.allow_null_values = item_dict['AllowNullValues']
-        self.__sql_nullable = None
         self.value_example = item_dict['ValueExample']
-        self.__set_sql_data_type_methods = [self.__set_date_for_iso8601,
-                                            self.__set_standard_values_sql,
-                                            self.__set_empty_value_types,
-                                            self.__set_date_sql_data_type,
-                                            self.__set_string_sql_data_type,
-                                            self.__set_int_sql_data_type,
-                                            self.__set_decimal_sql_data_type,
-                                            self.__set_bool_sql_data_type,
-                                            self.__set_binary_sql_data_type]
 
         if kwargs.get('clean', True):
             self.__check_reference_system(item_dict)
             self.__clean_standard_values_list()
             self.__clean_value_data_types()
-
-        self.__set_sql_data_types()
 
     def __check_reference_system(self, item_dict):
         if self.reference_system_for_values is None:
@@ -47,99 +34,6 @@ class O3Attribute(O3Element):
         for i, item in enumerate(self.standard_values_list):
             if 'Reference System' in item.value_name or 'Current ICD standard' in item.value_name:
                 self.standard_values_list.pop(i)
-
-    @property
-    def __sql_field_name(self):
-        return ''.join(self.string_code.split('_')[1:])
-
-    def __set_sql_nullable(self, **kwargs):
-        _phi = kwargs.get('PHI', True)
-
-        if self.allow_null_values in ['Yes', 'True', True, 'Yes, if diagnosis is for secondary cancer',
-                                      'Yes, except when intervention is TURP']:
-            self.__sql_nullable = 'NULL'
-
-        if self.allow_null_values in ['No']:
-            self.__sql_nullable = 'NOT NULL'
-
-        if _phi:
-            if self.allow_null_values == 'No for systems alloing PHI. Yes for systems not allowing PHI':
-                self.__sql_nullable = 'NOT NULL'
-            if self.allow_null_values == 'No for systems allowing PHI. Yes for systems not allowing PHI':
-                self.__sql_nullable = 'NOT NULL'
-            if self.allow_null_values == 'Yes for systems allowing PHI. No for systems not allowing PHI':
-                self.__sql_nullable = 'NULL'
-            if self.string_code == 'Patient_MRN':
-                self.__sql_nullable = 'NOT NULL'
-            if self.string_code == 'Patient_AnonPatID':
-                self.__sql_nullable = 'NULL'
-        else:
-            if self.allow_null_values == 'No for systems alloing PHI. Yes for systems not allowing PHI':
-                self.__sql_nullable = 'NULL'
-            if self.allow_null_values == 'No for systems allowing PHI. Yes for systems not allowing PHI':
-                self.__sql_nullable = 'NULL'
-            if self.allow_null_values == 'Yes for systems allowing PHI. No for systems not allowing PHI':
-                self.__sql_nullable = 'NOT NULL'
-            if self.string_code == 'Patient_MRN':
-                self.__sql_nullable = 'NULL'
-            if self.string_code == 'Patient_AnonPatID':
-                self.__sql_nullable = 'NOT NULL'
-
-        if self.__sql_nullable is None:
-            warnings.warn(f"No SQL nullable field set using logic. Defaulting to NULL for {self}")
-            self.__sql_nullable = 'NULL'
-
-    def __sql_field_type(self, sql_server):
-        if sql_server not in self.__sql_data_types.keys():
-            raise KeyError(f"Provided SQL server {sql_server} is not supported. Only MSSQL and PSQL are supported.")
-        data_types = self.__sql_data_types[sql_server]
-        return data_types[self.sql_data_type]
-
-    def __set_date_for_iso8601(self):
-        if self.reference_system_for_values is None:
-            return
-        if 'ISO 8601' in self.reference_system_for_values:
-            self.sql_data_type = 'Date'
-
-    def __set_standard_values_sql(self):
-        if len(self.standard_values_list) > 0:
-            self.sql_data_type = "String"
-
-    def __set_empty_value_types(self):
-        if self.value_data_type == "":
-            self.sql_data_type = "String"
-
-    def __set_date_sql_data_type(self):
-        if "date".lower() in self.value_name.lower():
-            self.sql_data_type = "Date"
-        if self.value_data_type == "Date":
-            self.sql_data_type = "Date"
-
-    def __set_string_sql_data_type(self):
-        if "String".lower() == self.value_data_type.lower():
-            self.sql_data_type = "String"
-
-    def __set_binary_sql_data_type(self):
-        if "DICOM".lower() in self.value_data_type.lower():
-            self.sql_data_type = "Binary"
-
-    def __set_int_sql_data_type(self):
-        if "Integer" in self.value_data_type or "Int" in self.value_data_type:
-            self.sql_data_type = "Integer"
-
-    def __set_bool_sql_data_type(self):
-        if "Boolean".lower() == self.value_data_type.lower():
-            self.sql_data_type = "Boolean"
-
-    def __set_decimal_sql_data_type(self):
-        if "Decimal" in self.value_data_type or "Numeric" in self.value_data_type:
-            self.sql_data_type = "Decimal"
-
-    def __set_sql_data_types(self):
-        for _method in self.__set_sql_data_type_methods:
-            _method()
-            if self.sql_data_type is not None:
-                break
 
     def __clean_value_data_types(self):
         if len(self.standard_values_list) > 0:
@@ -158,10 +52,6 @@ class O3Attribute(O3Element):
                 self.value_data_type = "Date"
             if self.value_data_type == "string":
                 self.value_data_type = "String"
-
-    def create_sql_field_text(self, sql_server, **kwargs):
-        self.__set_sql_nullable(**kwargs)
-        return f'{self.__sql_field_name} {self.__sql_field_type(sql_server)} {self.__sql_nullable}'
 
 
 if __name__ == "__main__":
---
+++ src/sql_interface/attribute_to_column.py
@@ -0,0 +1,128 @@
+import warnings
+
+from helpers.enums import SupportedSQLServers
+from src.base.o3_attribute import O3Attribute
+from src.sql_interface.sql_type_from_o3_data_type import sql_data_types
+
+
+class AttributeToSQLColumn:
+    def __init__(self, attribute: O3Attribute, allow_phi_flag: bool, sql_server_type: SupportedSQLServers):
+        if sql_server_type not in SupportedSQLServers:
+            raise KeyError(f"Provided SQL server {sql_server_type} is not supported. "
+                           f"Only MSSQL and PSQL are supported.")
+
+        self.attribute = attribute
+        self.allow_phi = allow_phi_flag
+        self.sql_server = sql_server_type
+        self.column_data_type = None
+        self.column_nullable = None
+        self.__set_data_type_methods = [self.__set_date_for_iso8601,
+                                        self.__set_standard_values_data_type,
+                                        self.__set_empty_value_types,
+                                        self.__set_date_data_type,
+                                        self.__set_string_data_type,
+                                        self.__set_int_data_type,
+                                        self.__set_decimal_data_type,
+                                        self.__set_bool_data_type,
+                                        self.__set_binary_data_type]
+        self.__set_data_types()
+        self.__set_nullable()
+
+    @property
+    def column_name(self):
+        return ''.join(self.attribute.string_code.split('_')[1:])
+
+    @property
+    def column_creation_text(self):
+        return f'{self.column_name} {self.column_data_type(self.sql_server)} {self.column_nullable}'
+
+    @property
+    def __sql_field_type(self):
+        data_types = sql_data_types[self.sql_server]
+        return data_types[self.sql_data_type]
+
+    def __set_nullable(self):
+
+        if self.attribute.allow_null_values in ['Yes', 'True', True, 'Yes, if diagnosis is for secondary cancer',
+                                                'Yes, except when intervention is TURP']:
+            self.column_nullable = 'NULL'
+
+        if self.attribute.allow_null_values in ['No']:
+            self.column_nullable = 'NOT NULL'
+
+        if self.allow_phi:
+            if self.attribute.allow_null_values == 'No for systems alloing PHI. Yes for systems not allowing PHI':
+                self.column_nullable = 'NOT NULL'
+            if self.attribute.allow_null_values == 'No for systems allowing PHI. Yes for systems not allowing PHI':
+                self.column_nullable = 'NOT NULL'
+            if self.attribute.allow_null_values == 'Yes for systems allowing PHI. No for systems not allowing PHI':
+                self.column_nullable = 'NULL'
+            if self.attribute.string_code == 'Patient_MRN':
+                self.column_nullable = 'NOT NULL'
+            if self.attribute.string_code == 'Patient_AnonPatID':
+                self.column_nullable = 'NULL'
+        else:
+            if self.attribute.allow_null_values == 'No for systems alloing PHI. Yes for systems not allowing PHI':
+                self.column_nullable = 'NULL'
+            if self.attribute.allow_null_values == 'No for systems allowing PHI. Yes for systems not allowing PHI':
+                self.column_nullable = 'NULL'
+            if self.attribute.allow_null_values == 'Yes for systems allowing PHI. No for systems not allowing PHI':
+                self.column_nullable = 'NOT NULL'
+            if self.attribute.string_code == 'Patient_MRN':
+                self.column_nullable = 'NULL'
+            if self.attribute.string_code == 'Patient_AnonPatID':
+                self.column_nullable = 'NOT NULL'
+
+        if self.column_nullable is None:
+            warnings.warn(f"No SQL nullable field set using logic. Defaulting to NULL for {self}")
+            self.column_nullable = 'NULL'
+
+    def __set_date_for_iso8601(self):
+        if self.attribute.reference_system_for_values is None:
+            return
+        if 'ISO 8601' in self.attribute.reference_system_for_values:
+            self.sql_data_type = 'Date'
+
+    def __set_standard_values_data_type(self):
+        if len(self.attribute.standard_values_list) > 0:
+            self.sql_data_type = "String"
+
+    def __set_empty_value_types(self):
+        if self.attribute.value_data_type == "":
+            self.sql_data_type = "String"
+
+    def __set_date_data_type(self):
+        if "date".lower() in self.attribute.value_name.lower():
+            self.sql_data_type = "Date"
+        if self.attribute.value_data_type == "Date":
+            self.sql_data_type = "Date"
+
+    def __set_string_data_type(self):
+        if "String".lower() == self.attribute.value_data_type.lower():
+            self.sql_data_type = "String"
+
+    def __set_binary_data_type(self):
+        if "DICOM".lower() in self.attribute.value_data_type.lower():
+            self.sql_data_type = "Binary"
+
+    def __set_int_data_type(self):
+        if "Integer" in self.attribute.value_data_type or "Int" in self.attribute.value_data_type:
+            self.sql_data_type = "Integer"
+
+    def __set_bool_data_type(self):
+        if "Boolean".lower() == self.attribute.value_data_type.lower():
+            self.sql_data_type = "Boolean"
+
+    def __set_decimal_data_type(self):
+        if "Decimal" in self.attribute.value_data_type or "Numeric" in self.attribute.value_data_type:
+            self.sql_data_type = "Decimal"
+
+    def __set_data_types(self):
+        for _method in self.__set_data_type_methods:
+            _method()
+            if self.sql_data_type is not None:
+                break
+
+
+if __name__ == "__main__":
+    pass
